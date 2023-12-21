dc3345ece03c39a12219ca0c8b857961124492ea
The0x539 <the0x539@gmail.com>
2023-12-21 01:50:33 -0600

    day 21 part 2 ready to be solved

--- day21/src/main.rs
+++ day21/src/main.rs
@@ -175,36 +175,7 @@ impl Memory {
     }
 }
 
-const NESW: [(i32, i32); 4] = [(0, -1), (1, 0), (0, 1), (-1, 0)];
-
-fn part2(n: &[In]) -> Out {
-    let (start, grid) = setup(n);
-
-    let mut memory = Memory::default();
-    let blank = memory.intern(Tile::new());
-
-    let mut tiles: BTreeMap<P, usize> = BTreeMap::new();
-    tiles.insert(P::new(0, 0), memory.intern(Tile::from([start])));
-
-    let steps = if cfg!(test) { 1000 } else { 26501365 };
-    let steps = 5000;
-
-    for _i in 0..steps {
-        tiles.retain(|_, t| *t != blank);
-        for pu in Vec::from_iter(tiles.keys().copied()) {
-            for dir in NESW {
-                tiles.entry(pu + dir).or_insert(blank);
-            }
-        }
-
-        let mut new_tiles = BTreeMap::new();
-        for (&pu, &tile) in &tiles {
-            let neighbors = NESW.map(|dir| *tiles.get(&(pu + dir)).unwrap_or(&blank));
-            let new_tile = memory.simulate(tile, neighbors, &grid);
-            new_tiles.insert(pu, new_tile);
-        }
-        tiles = new_tiles;
-    }
+fn print_interned_tiles(tiles: &BTreeMap<P, usize>) {
     let x0 = tiles.keys().map(|k| k.x).min().unwrap();
     let x1 = tiles.keys().map(|k| k.x).max().unwrap();
     let y0 = tiles.keys().map(|k| k.y).min().unwrap();
@@ -220,8 +191,82 @@ fn part2(n: &[In]) -> Out {
         }
         println!();
     }
+}
 
-    println!("{} {}", memory.hits, memory.misses);
+const NESW: [(i32, i32); 4] = [(0, -1), (1, 0), (0, 1), (-1, 0)];
+
+fn part2(n: &[In]) -> Out {
+    let (start, grid) = setup(n);
+
+    let mut memory = Memory::default();
+    let blank = memory.intern(Tile::new());
+
+    let mut tiles: BTreeMap<P, usize> = BTreeMap::new();
+    tiles.insert(P::new(0, 0), memory.intern(Tile::from([start])));
+
+    let steps = if cfg!(test) { 1000 } else { 26501365 };
+    let steps = 1000;
+
+    let mut last_growth = 0;
+
+    for i in 0..steps {
+        tiles.retain(|_, t| *t != blank);
+        let old_len = tiles.len();
+
+        for pu in Vec::from_iter(tiles.keys().copied()) {
+            for dir in NESW {
+                tiles.entry(pu + dir).or_insert(blank);
+            }
+        }
+
+        let mut new_tiles = BTreeMap::new();
+        for (&pu, &tile) in &tiles {
+            let neighbors = NESW.map(|dir| *tiles.get(&(pu + dir)).unwrap_or(&blank));
+            let new_tile = memory.simulate(tile, neighbors, &grid);
+            new_tiles.insert(pu, new_tile);
+        }
+        tiles = new_tiles;
+
+        tiles.retain(|_, t| *t != blank);
+        let new_len = tiles.len();
+
+        if new_len > old_len {
+            println!(
+                "grew {old_len} -> {new_len} at {i} ({} since last)",
+                i - last_growth
+            );
+            last_growth = i;
+            // print_interned_tiles(&tiles);
+
+            if new_len == 25 {
+                // .    .    .    68   .    .    .
+                // .    .    916  919  921  .    .
+                // .    916  1692 384  1694 921  .
+                // 67   917  384  389  384  922  71      (i=327)
+                // .    918  1693 384  1695 923  .
+                // .    .    918  920  923  .    .
+                // .    .    .    70   .    .    .
+                //
+                // extrapolates to
+                //
+                // .    .    .    .    .    68   .    .    .    .    .
+                // .    .    .    .    916  919  921  .    .    .    .
+                // .    .    .    916  1692 384  1694 921  .    .    .
+                // .    .    916  1692 384  389  384  1694 921  .    .
+                // .    916  1692 384  389  384  389  384  1694 921  .
+                // 67   917  384  389  384  389  384  389  384  922  71 (i=589)
+                // .    918  1693 384  389  384  389  384  1695 923  .
+                // .    .    918  1693 384  389  384  1695 923  .    .
+                // .    .    .    918  1693 384  1695 923  .    .    .
+                // .    .    .    .    918  920  923  .    .    .    .
+                // .    .    .    .    .    70   .    .    .    .    .
+                //
+                // but we probably want to avoid performing even a single simulation step after extrapolating
+                // so figure out which part of the cycle is exactly 131*n steps before the end
+                todo!();
+            }
+        }
+    }
 
     tiles
         .values()
